d14445f34955f9399b3a8cc4363fbccf8d180b1b
gonejack <igonejack@gmail.com>
2021-06-02 11:58:11 +0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
-int main() {
 
+#include <cstdio>
+#include <loguru.hpp>
+
+int main() {
+    LOG_F(INFO, "first %d", 200);
+    LOG_F(INFO, "first");
+    LOG_F(INFO, "first %d", 300);
+    LOG_F(INFO, "first %s", "输入法字符串");
 }
